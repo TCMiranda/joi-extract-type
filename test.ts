753123ae59c9858ff243670610d3d67110572fb8
Jason Ibrahim <jasonaibrahim@gmail.com>
2021-11-02 17:29:40 -0700

    refactor: change import

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 /** @format */
 
-import JoiExtractType from './index';
 import { jobOperatorRoleSchema } from './index.spec';
+import extractType from './index';
 
-export type expectedType = JoiExtractType.extractType<typeof jobOperatorRoleSchema>;
+export type expectedType = extractType<typeof jobOperatorRoleSchema>;
 
 export const response = ((unpredictableData: expectedType) => {
   const validation = unpredictableData.validate(jobOperatorRoleSchema);
